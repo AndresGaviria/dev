7fe54f66317999b89b19d25a43b768d38c0851f5
Andres <anacleto115@hotmail.com>
2025-02-14 16:04:48 -0500

    Test

--- lib_entities/obj/Debug/net8.0/lib_entities.AssemblyInfo.cs
+++ lib_entities/obj/Debug/net8.0/lib_entities.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("lib_entities")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df8abd4e38ea801b0cf512af131c12a4075aa7e6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+202c58843e34eec970115bdf9654f67680f6482f")]
 [assembly: System.Reflection.AssemblyProductAttribute("lib_entities")]
 [assembly: System.Reflection.AssemblyTitleAttribute("lib_entities")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
